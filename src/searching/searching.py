16032fabe855fe7053110c35ff086f5f159d8bd9
Trevor Thompson <Thompson_Trevor@bah.com>
2020-07-09 22:20:15 -0400

    Successfully fixed linear search code

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,23 +1,28 @@
-def linear_search(arr, target):
-    for i in range(len(arr - 1)):
-    if i == target:
-        return 1
+import math
 
-    return -1   # not found
+def linear_search(arr, target):
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
     start = 0
     end = len(arr) - 1
-    mid = ((end+start)/2) + 1
+
+    if(len(arr)-1)%2 == 0:
+        mid = math.ceil((end+start)/2+1)
+    else:
+        mid = math.ceil((end+start)/2-1)
+
     if(target < arr[mid]):
        end = mid
     elif(target > arr[mid]):
        start = mid
     else:
-        return 1
-    # Your code here
-
+        return mid
 
     return -1  # not found
